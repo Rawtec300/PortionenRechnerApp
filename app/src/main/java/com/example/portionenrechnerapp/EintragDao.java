6b11e610377eab5f5ccd08cf9778ec375f2c4030
FaNaSKY <55694295+FaNaSKY@users.noreply.github.com>
2019-10-23 00:09:49 +0200

    Create EintragDao.java
    Dao Grundbasis erstellt

---
+++ app/src/main/java/com/example/portionenrechnerapp/EintragDao.java
@@ -0,0 +1,23 @@
+package com.example.portionenrechnerapp;
+
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface EintragDao {
+
+    @Insert(OnConflict = OnConflictStrategy.REPLACE)
+    public void insert(Eintrag eintrag);
+
+    @Query("SELECT * FROM Eintrag")
+    public List<Eintrag> getAll();
+
+    @Delete
+    public void delete(Eintrag eintrag);
+}
