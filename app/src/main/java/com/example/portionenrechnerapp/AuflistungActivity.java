d097c26ec58e88449c6fc65ebbfc6ce6a0471e7d
Noah Colby <noah.colby@yahoo.de>
2019-10-27 21:10:49 +0100

    folien durchgearbeitet
    Fehler: Beim berechnen stürzen wir ab

--- app/src/main/java/com/example/portionenrechnerapp/AuflistungActivity.java
+++ app/src/main/java/com/example/portionenrechnerapp/AuflistungActivity.java
@@ -1,7 +1,62 @@
 package com.example.portionenrechnerapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class AuflistungActivity extends AppCompatActivity {
-    
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class AuflistungActivity extends AppCompatActivity implements View.OnClickListener {
+    private EintragDao dao;
+    private RecyclerView recyclerView;
+    private EintragListAdapter adapter;
+    Button buttonZeigeListe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database_list);
+
+        recyclerView = findViewById(R.id.eintraege_recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new EintragListAdapter();
+        recyclerView.setAdapter(adapter);
+
+        dao = EintragDatabase.getDatabase(this).eintragDao();
+        buttonZeigeListe = findViewById(R.id.button_anzeigen);
+        buttonZeigeListe.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        if(view == buttonZeigeListe){
+            Intent intent_anzeigen = new Intent(this, AuflistungActivity.class);
+            startActivity(intent_anzeigen);
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new LadeEintraegeTask().execute();
+    }
+
+    public class LadeEintraegeTask extends AsyncTask<View, View, List<Eintrag>> {
+
+        @Override
+        protected List<Eintrag> doInBackground(View... views) {
+            return dao.getAll();
+        }
+
+        @Override
+        protected void onPostExecute(List<Eintrag> eintraege) {
+            super.onPostExecute(eintraege);
+            adapter.setEintraege(eintraege);
+        }
+    }
 }
