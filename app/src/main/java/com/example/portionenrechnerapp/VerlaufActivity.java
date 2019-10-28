8521d7ece58bde59ce3dc5e01c29be7547d4a7bf
FaNaSKY <55694295+FaNaSKY@users.noreply.github.com>
2019-10-28 11:01:27 +0100

    RecyclerView vervollständigt (not working)

--- app/src/main/java/com/example/portionenrechnerapp/VerlaufActivity.java
+++ app/src/main/java/com/example/portionenrechnerapp/VerlaufActivity.java
@@ -5,10 +5,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
+
+import java.util.ArrayList;
 
 public class VerlaufActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     RecyclerView.Adapter adapter;
+    ArrayList<String> eintraege;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +21,14 @@ public class VerlaufActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recycler_view);
 
+        eintraege = new ArrayList<>();
+
+        for (int i = 0; i <10; i++) {
+            eintraege.add("Test Nr: "+i);
+        }
+
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new EintragAdapter();
+        adapter = new EintragAdapter(eintraege);
         recyclerView.setAdapter(adapter);
     }
 }
